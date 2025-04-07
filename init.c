9ae67778130d87144b52e768d76699b8e3fd832e
jikopp <elsoandroid@gmail.com>
2025-04-07 11:39:06 +0200

    added child process and updated execute cmd

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: ferenc <ferenc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 23:36:05 by elavrich          #+#    #+#             */
-/*   Updated: 2025/04/07 10:42:19 by ferenc           ###   ########.fr       */
+/*   Updated: 2025/04/07 11:37:26 by ferenc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,51 @@ void	init_shell(t_shell *shell, char **envp)
 	shell->env_var = copy_envp(envp);
 	//print_env(*shell); // test printing
 }
+
+void	child_process(t_shell *shell, char **cmd, char *path) // f:07/04/25 - we will need more space for pipe
+{
+	if (execve(path, cmd, shell->env_var) == -1)
+	{
+		perror("execve failed");
+		exit(EXIT_FAILURE);
+	}
+}
+void	execute_cmd(char **cmd, t_shell *shell)
+{
+	char	*path;
+	pid_t	pid;
+
+	path = get_cmd_path(cmd[0], shell);
+	if (!path)
+	{
+		perror("Command not found");
+		return;
+	}
+	pid = fork();
+	if (pid == -1)
+		perror("fork");
+	else if (pid == 0)
+		child_process(shell, cmd, path);
+	else
+		waitpid(pid, NULL, 0);
+	free(path);
+}
+
 void	take_comm(t_token *tokens, t_shell *shell)
 {
 	char	*command;
 	char	**cmd;
-	char	*path;
+	pid_t	pid;
 
 	command = readline("prompt> ");
 	if (!command) // f:07/04/25 - ctrl - d - otherwise runs to seg fault, we need to handle this later
 		return;
 	input(command, &tokens);
 	cmd = make_args(tokens);
-	if(cmd) //basic version, will prob need to change later.
-	{
-		path = get_cmd_path(cmd[0], shell);
-		if (!path)
-			free_array(cmd);
-		else 
-		{
-			if(execve(path, cmd, shell->env_var) == -1)
-				free_array(cmd);
-		}
-		
-	}
+	if (!cmd)
+		return;
+	execute_cmd(cmd, shell);
+	free_array(cmd);
 	//int j = 0;//testing 
 	// while (cmd[j]) 
 	// {
