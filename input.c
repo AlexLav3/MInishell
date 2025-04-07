aed39ffd4962f9d4202ddfdce22668cde786e4e2
jikopp <elsoandroid@gmail.com>
2025-04-07 17:24:34 +0200

    removed extra free

--- input.c
+++ input.c
@@ -6,7 +6,7 @@
 /*   By: ferenc <ferenc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 00:29:49 by elavrich          #+#    #+#             */
-/*   Updated: 2025/04/07 16:55:12 by ferenc           ###   ########.fr       */
+/*   Updated: 2025/04/07 17:23:36 by ferenc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	input(char *str, t_token **tokens)
 			return ;
 		ft_strlcpy(word, &str[start], word_len + 1);
 		add_token(tokens, word);
-		free(word);
+		// free(word);
 	}
 	//print_list(*tokens); //test to check what's inside the tokens
 	//free(word); //doesn't change anything in the memory leaks but creates issues.
