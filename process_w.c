b71c1b52c49c986495eb5770f769cdf944b35c10
Alex <alexlav.elly@gmail.com>
2025-07-09 22:41:30 +0200

    small fix

--- process_w.c
+++ process_w.c
@@ -6,7 +6,7 @@
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 18:23:43 by elavrich          #+#    #+#             */
-/*   Updated: 2025/06/25 00:24:00 by elavrich         ###   ########.fr       */
+/*   Updated: 2025/07/09 22:40:38 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*process_word(char *word, t_shell *shell, int flag)
 	{
 		exp = handle_dollar(word, shell);
 		if (!exp)
-			return (free(word), ft_strdup(""));
+			return (word);
 	}
 	else
 		return (word);
