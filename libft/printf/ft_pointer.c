bbba1e11435c6f11df5a8c4cc4f243c7e16d8edf
jikopp <elsoandroid@gmail.com>
2025-07-21 19:56:58 +0200

    fixed makefile error due to libft makefile

--- libft/printf/ft_pointer.c
+++ libft/printf/ft_pointer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pointer.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ferenc <ferenc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 15:37:47 by elavrich          #+#    #+#             */
-/*   Updated: 2024/10/01 20:20:51 by elavrich         ###   ########.fr       */
+/*   Updated: 2025/07/21 19:53:19 by ferenc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ unsigned long	ft_ptr(unsigned long num)
 unsigned long	ft_vptr(unsigned long num, const char *format)
 {
 	unsigned long	value;
-	char			*symbols;
+	// char			*symbols;
 
 	(void)format;
-	symbols = "0123456789abcdef";
+	// symbols = "0123456789abcdef";
 	value = 0;
 	if (!num)
 		value += write(1, "(nil)", 5);
