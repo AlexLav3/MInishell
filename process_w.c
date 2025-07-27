898d8793fddc619ee7b551ea4707439023b52b37
Alex <alexlav.elly@gmail.com>
2025-07-27 22:26:20 +0200

    dollar:

--- process_w.c
+++ process_w.c
@@ -6,7 +6,7 @@
 /*   By: elavrich <elavrich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 18:23:43 by elavrich          #+#    #+#             */
-/*   Updated: 2025/07/25 23:00:22 by elavrich         ###   ########.fr       */
+/*   Updated: 2025/07/27 22:25:56 by elavrich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 char	*process_word(char *word, t_shell *shell, int flag)
 {
 	char	*exp;
+	int		len;
 
+	len = 0;
+	printf("word: %s\n", word);
 	if (flag != EXPAND || !word)
 		return (word);
 	if (ft_strchr(word, '$') != NULL)
 	{
+		len = ft_strlen(word);
+		if (word[len - 1] == '$')
+			return (word);
 		exp = handle_dollar(word, shell);
 		if (!exp)
 			return (word);
